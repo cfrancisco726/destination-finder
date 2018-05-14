c20d76bb29bb0d8a2eb05d82f824c89efaa06cf6
cfrancisco726 <cfrancisco726@gmail.com>
2018-05-14 17:20:12 -0400

    set up prod

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+	googleClientID:
+		'315335872082-m0gts1v6jhqpk3dril2u5oufm5n092fr.apps.googleusercontent.com',
+	googleClientSecret: '2M54_35FV6oB90f1zxxB8H6x',
+	mongoURI:
+		'mongodb://cfrancisco26:msmlab26@ds119800.mlab.com:19800/destination-dev',
+	cookieKey: 'alksjfkajdfljasdfjalsdfjlasdjfljsdf'
+};
