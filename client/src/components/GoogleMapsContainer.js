0added6ba3dee2db310a1c4f37a666f552e285ce
cfrancisco726 <cfrancisco726@gmail.com>
2019-04-23 18:01:02 -0400

    map zoom updated to 2

--- client/src/components/GoogleMapsContainer.js
+++ client/src/components/GoogleMapsContainer.js
@@ -126,7 +126,7 @@ class GoogleMapsContainer extends Component {
 					style={style}
 					google={this.props.google}
 					onClick={this.onMapClick}
-					zoom={1.5}
+					zoom={2}
 					initialCenter={{ lat: '41.850033', lng: '-87.6500523' }}
 				>
 					{!this.props.trips
